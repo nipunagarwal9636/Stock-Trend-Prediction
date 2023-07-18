811715c68e8679fa386fd09d3158ffe207553459
nipunagarwal9636 <58950264+nipunagarwal9636@users.noreply.github.com>
2023-07-19 00:47:50 +0530

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -45,7 +45,7 @@ model = load_model('keras_model.h5')
 
 data_training.tail(100)
 past_100_days = data_training.tail(100)
-final_df = past_100_days.append(data_testing, ignore_index = True)
+final_df = pd.concat([past_100_days, data_testing],ignore_index=True)
 input_data = scaler.fit_transform(final_df)
 
 x_test= []
