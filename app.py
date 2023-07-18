08d9de88e322fdf09348f6ce520cdfa2fc04bc01
nipunagarwal9636 <58950264+nipunagarwal9636@users.noreply.github.com>
2023-07-18 23:33:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 import yfinance as yf
 import matplotlib.pyplot as plt
-from keras.model import load_model
+from keras.models import load_model
 import streamlit as st
 
 st.title("Stock Trend Prediction")
